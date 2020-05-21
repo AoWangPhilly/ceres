c7fd811558b67c478317c5f2bb7d725f7c50af51
Ao Wang <aw3338@drexel.edu>
2020-05-21 20:23:11 +0000

    Update wildfire data

--- map_data/DataRunner.py
+++ map_data/DataRunner.py
@@ -11,37 +11,47 @@ from CleanData import CleanData, get_week_time_coordinates
 from GitCommand import AutomateCommit
 import pandas as pd
 import schedule
+import datetime
 import time
 
 
 def collectAndClean():
     # Initalize three CleanData objects for MODIS, viirs, and noaa
+    print("Web scraping NASA satellite data...", end="")
     modis, viirs, noaa = CleanData(sensor="MODIS"), CleanData(
         sensor="viirs"), CleanData(sensor="noaa")
+    print("DONE!! At {}".format(datetime.datetime.now()))
 
     # Combines the three dataframes
+    print("Combining and cleaning...", end="")
     collected = modis.combine_data_sets(viirs, noaa)
+    print("DONE!! At {}".format(datetime.datetime.now()))
 
     # Gets the fire radiative power from the combined dataframe and
     # saves it as a CSV file
+    print("Gathering FRP data...", end="")
     frp_series = pd.DataFrame(collected.frp[collected.frp.notna()])
     frp_series.columns = ["FRP"]
     frp_series.to_csv(
         "/home/aow252/ceres-bucket-1/ceres/website/frp.csv", index=False)
+    print("DONE!! At {}".format(datetime.datetime.now()))
 
+    print("Mapping wildfire data...", end="")
     # Gets the mapping of coordinates of wildfires per day and saves it
     # as a JSON file
     plot_data = get_week_time_coordinates(collected)
     with open("/home/aow252/ceres-bucket-1/ceres/website/week_data.json", "w") as data:
         data.write(str(plot_data).replace("'", '"'))
-
+    print("DONE!! At {}".format(datetime.datetime.now()))
 
 def runGit():
+    print("Committing and Pushing...", end="")
     # Initialize the object to run git commands
     command = AutomateCommit(commit_message="Update wildfire data",
                              repo_dir="/home/aow252/ceres-bucket-1/ceres/")
     command.git_commit()
     command.git_push()
+    print("DONE!! At {}".format(datetime.datetime.now()))
 
 
 def main():
@@ -50,7 +60,8 @@ def main():
 
 
 # Runs the script every 12 hours
-schedule.every(12).hours.do(main)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#schedule.every(12).hours.do(main)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
+main()
